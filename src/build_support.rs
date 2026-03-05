0e7c3a8892e0bacd5a277ba6db6ac240a46c2aff
Jason Taylor <jason@originaljt.com>
2026-03-04 22:21:03 -0800

    additional clippy fix

--- src/build_support.rs
+++ src/build_support.rs
@@ -80,7 +80,7 @@ fn compute_git_hash() -> String {
 }
 
 fn is_leap(y: u64) -> bool {
-   y % 4 == 0 && (y % 100 != 0 || y % 400 == 0)
+   y.is_multiple_of(4) && (!y.is_multiple_of(100) || y.is_multiple_of(400))
 }
 
 fn days_in_year(y: u64) -> u64 {
